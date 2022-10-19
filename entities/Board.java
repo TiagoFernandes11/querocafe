705a1cc5cd7a7301f0a9399c2f2e2789c8c8fadf
Tiago Fernandes <tiagofernandesribeiro@yahoo.com.br>
2022-10-19 09:57:09 -0300

    testing

--- entities/Board.java
+++ entities/Board.java
@@ -1,5 +1,9 @@
 package entities;
 
+import java.util.Arrays;
+
+import javax.swing.text.Position;
+
 public class Board {
     //  1    2    3
     // a1 | a2 | a3  a
@@ -8,6 +12,60 @@ public class Board {
     // ------------
     // c1 | c2 | c3  c
 
+    private Position[][] board;
+
+
+
+    public Board(Position[][] board) {
+        this.board = board;
+    }
+
+
+    public Position[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(Position[][] board) {
+        this.board = board;
+    }
+
+
+    @Override
+    public String toString() {
+        int[] column = new int[board.length];
+        int[] row = new int[board.length];
+
+
+        for(int i = 0 ; i < column.length; i++){
+
+                for(int j = 0; j < row.length; j++){
+                     if(j< 2){
+                        System.out.print(" " + board[i][j] + " |");
+                     }
+                     else if(j==2){
+                        System.out.println(" " + board[i][j]);
+                     }
+                     else if(i< 2){
+                        System.out.println("------------");
+                     }
+                }
+
+                // a1 | a2 | a3  a
+                // ------------
+
+        }
+
+
+
+
+
+
+        return "Board [board=" + Arrays.toString(board) + "]";
+    }
+
+
+    
     
 
+    
 }
